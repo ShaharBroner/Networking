ce147318dc5bcf1750bf24f0a20880615b78d7f5
Shahar <shahar.broner@gmail.com>
2019-04-02 15:00:55 -0700

    Initial commit

---
+++ src/DateServer.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class DateServer {
+    private static final int PORT = 9090;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(PORT);
+        System.out.println("Server is waiting for client connection");
+        Socket client = listener.accept();
+        System.out.println("Server connected to client");
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        out.println((new Date()).toString());
+        System.out.println("Server sent date, closing");
+        client.close();
+        listener.close();
+    }
+}
