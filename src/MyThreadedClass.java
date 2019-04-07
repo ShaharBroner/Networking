dedfb8df988d3e3032d77f44f0b7b5165a7f3491
Shahar <shahar.broner@gmail.com>
2019-04-07 16:34:25 -0700

    Changed to support threads

---
+++ src/MyThreadedClass.java
@@ -0,0 +1,15 @@
+public class MyThreadedClass implements Runnable {
+    private static final int MAX = 100000;
+    private String id;
+
+    public MyThreadedClass(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < MAX; i++) {
+            System.out.println(id + " is on loop #" + i);
+        }
+    }
+}
