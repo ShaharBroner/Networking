82deb807ce9ea3f840ccb6bd7d9cbe6b2bddf691
Shahar <shahar.broner@gmail.com>
2019-04-07 16:33:30 -0700

    Changed to support threads

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Client {
@@ -10,9 +11,16 @@ public class Client {
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket(SERVER_IP, SERVER_PORT);
-        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        String serverResponse = input.readLine();
-        JOptionPane.showMessageDialog(null, serverResponse);
+        ServerConnection serverConn = new ServerConnection(socket);
+        BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        new Thread(serverConn).start();
+        while (true) {
+            System.out.println("> ");
+            String command = keyboard.readLine();
+            if (command.equals("quit")) break;
+            out.println(command);
+        }
         socket.close();
         System.exit(0);
     }
