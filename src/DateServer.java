5cc3379030bd5a9428ce1bd7cb1be3007e05a47a
Shahar <shahar.broner@gmail.com>
2019-04-07 16:33:43 -0700

    Changed to support threads

--- src/DateServer.java
+++ src/DateServer.java
@@ -1,21 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DateServer {
+    private static String[] names = {"Jeff", "Bob", "Chungus", "Yertle"};
+    private static String[] adjs = {"the little", "the interesting", "the great", "the amazing"};
     private static final int PORT = 9090;
+    private static ArrayList<ClientHandler> clients = new ArrayList<>();
+    private static ExecutorService pool = Executors.newFixedThreadPool(4);
 
     public static void main(String[] args) throws IOException {
         ServerSocket listener = new ServerSocket(PORT);
-        System.out.println("Server is waiting for client connection");
-        Socket client = listener.accept();
-        System.out.println("Server connected to client");
-        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-        out.println((new Date()).toString());
-        System.out.println("Server sent date, closing");
-        client.close();
-        listener.close();
+        while (true) {
+            System.out.println("Server is waiting for client connection");
+            Socket client = listener.accept();
+            System.out.println("Server connected to client");
+            ClientHandler clientThread = new ClientHandler(client,clients);
+            clients.add(clientThread);
+            pool.execute(clientThread);
+        }
+    }
+
+    public static String getRandomName() {
+        String name = names[(int) (Math.random() * names.length)];
+        String adj = adjs[(int) (Math.random() * adjs.length)];
+        return name + " " + adj;
     }
 }
